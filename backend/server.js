290ebb40f112b094bac62dc4c5dd72ae4a06439a
turtle <alessio.tartufoli@studenti.unicam.it>
2024-12-10 14:00:20 +0100

    backend funzionante con codice più modulare

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,17 @@ const connectDB = require('./src/config/db'); // Importa la funzione di connessi
 const swaggerUi = require('swagger-ui-express'); // Interfaccia Swagger
 const swaggerSpecs = require('./src/config/swaggerDef'); // Configurazione Swagger
 
-const Message = require('./src/models/Message'); // Modello per il salvataggio dei messaggi
-const User = require('./src/models/User'); //modello per il salvataggio degli utenti nel db 
+//const Message = require('./src/models/Message'); // Modello per il salvataggio dei messaggi
+//const User = require('./src/models/User'); //modello per il salvataggio degli utenti nel db 
 //const jwt = require('jsonwebtoken');//importa i moduli per la generazione del jwt
 
 // Importa il motore NLP
 const bodyParser = require('body-parser'); // Per elaborare il corpo delle richieste
-const RasaEngine = require('./src/nlp/rasaEngine'); // Classe per interagire con Rasa
+//const RasaEngine = require('./src/nlp/rasaEngine'); // Classe per interagire con Rasa
 console.log('NLP_ENGINE:', process.env.NLP_ENGINE);
 
 const BASE_URL = process.env.ENGINE_BASE_URL;
 console.log('ENGINE_BASE_URL:', process.env.ENGINE_BASE_URL);
-
-// Crea un'istanza di RasaEngine
-const rasaEngine = new RasaEngine(BASE_URL);
 
 // Crea l'app Express
 const app = express();
@@ -45,60 +42,21 @@ app.get('/', (req, res) => {
 
 
 
-
+//route per gestire le lezioni 
 const lezioniRoutes = require('./src/routes/lezioni'); // Importa il modulo delle routes
 app.use('/lezioni', lezioniRoutes); // Usa le routes
-
-//const chatRoutes = require('./src/routes/chat'); // Importa il modulo delle routes
-//app.use('/api/chat', chatRoutes); // Usa le routes
 
 
 const loginRoutes = require('./src/routes/loginRoutes');
 const signupRoutes = require('./src/routes/signupRoutes');
 
+//route per gestire l'autenticazione
 app.use('/api/auth', loginRoutes);
 app.use('/api/auth', signupRoutes);
 
-
-
-const messageRoutes = require('./src/routes/messageRoute');
-//const chatRoutes = require('./src/routes/chatRoute');
-
-
-// Endpoint per inviare per comunicare con il bot 
-app.post('/api/chat', async (req, res) => {
-  const { message } = req.body; // Estrarre il messaggio e il mittente dal corpo della richiesta
-  if (!message ) {
-    return res.status(400).json({ error: 'Message and sender are required' });
-  }
-  try {
-    const rasaResponse = await rasaEngine.sendMessage(message); // Invia il messaggio a Rasa
-    // Estrai il testo dalla risposta di Rasa
-    const botResponseText = rasaResponse[0]?.text || 'Errore nella risposta del bot';
-    // Salva il messaggio dell'utente nel database
-    const userMessage = new Message({
-    message: message, // Messaggio dell'utente
-    sender: 'user', // Indica che è un messaggio dell'utente
-    });
-      await userMessage.save(); // Salva nel database
-// Salva la risposta del bot nel database
-const botMessage = new Message({
-  message: botResponseText, // Risposta del bot
-  sender: 'bot', // Indica che è un messaggio del bot
-});
-await botMessage.save(); // Salva nel database
-    //res.json({ responses: botResponseText }); // Invia la risposta al client
-    res.json({ responses: [{ text: botResponseText }] });
-    console.log('Risposta inviata al frontend:', { responses: [{ text: botResponseText }] });
-  } catch (error) {
-    console.error("Errore durante la comunicazione con Rasa:", error.message);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-const chatRoutes = require('./src/routes/chat'); // Importa il modulo delle routes
-app.use('/api/chat', chatRoutes); // Usa le routes
-
+//route per inviare messaggi a Rasa
+const chatRoutes = require('./src/routes/chatRoute');
+app.use('/api/chat',chatRoutes);
 
 
 
